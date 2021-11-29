8eddff2dd3545a900e3b9e9a21b4f95aa48d0ec5
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-11-29 17:59:11 +0100

    🔖 increase version to 1.5.1
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ with open(README_PATH) as readme_file:
 
 setup(
     name='jkq.qmap',
-    version='1.5.0',
+    version='1.5.1',
     author='Lukas Burgholzer',
     author_email='lukas.burgholzer@jku.at',
     description='QMAP - A JKQ tool for Quantum Circuit Mapping',
