73771fecfea8c4e10023febf1bd52a3bf4af403d
Sarah Schneider <sarah.schneider@jku.at>
2022-06-10 17:38:29 +0200

    Added App file for cliffordsynthesis

---
+++ apps/cliffordsynthesis_app.cpp
@@ -0,0 +1,4 @@
+/*
+* This file is part of the MQT QMAP library which is released under the MIT license.
+* See file README.md or go to https://iic.jku.at/eda/research/ibm_qx_mapping/ for more information.
+*/
