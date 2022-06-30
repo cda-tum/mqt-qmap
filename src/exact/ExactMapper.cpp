7ecb9d59a32e753613f1b5299ca0075bf3e19821
Sarah Schneider <sarah.schneider@jku.at>
2022-06-30 09:40:30 +0200

    Fixed Codestyle

--- src/exact/ExactMapper.cpp
+++ src/exact/ExactMapper.cpp
@@ -611,7 +611,7 @@ void ExactMapper::coreMappingRoutine(const std::set<unsigned short>& qubitChoice
     piCount         = 0;
     internalPiCount = 0;
     std::vector<std::set<WeightedVar>> weightedVars(reducedLayerIndices.size());
-    LogicTerm cost = LogicTerm(0);
+    LogicTerm                          cost = LogicTerm(0);
     do {
         if (skipped_pi.count(piCount) == 0 || !config.enableSwapLimits) {
             auto picost = architecture.minimumNumberOfSwaps(pi);
