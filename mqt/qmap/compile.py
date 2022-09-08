0157cbd3dd63b560f9343dc66f4b3060255adc16
burgholzer <burgholzer@me.com>
2022-09-08 15:30:37 +0200

    🔥 remove unused imports
    Signed-off-by: burgholzer <burgholzer@me.com>

--- mqt/qmap/compile.py
+++ mqt/qmap/compile.py
@@ -2,8 +2,6 @@
 # This file is part of MQT QMAP library which is released under the MIT license.
 # See file README.md or go to http://iic.jku.at/eda/research/quantum_verification/ for more information.
 #
-import pickle
-from pathlib import Path
 from typing import List, Optional, Set, Tuple, Union
 
 from mqt.qmap.pyqmap import (
