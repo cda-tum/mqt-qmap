c2d449f36e4eabce43a94d52ce199357ab028c77
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-09-09 01:36:39 +0200

    🚸 more reliable `FindZ3.cmake`
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- cmake/FindZ3.cmake
+++ cmake/FindZ3.cmake
@@ -1,37 +1,6 @@
 # Code adapted from https://fossies.org/linux/llvm/cmake/modules/FindZ3.cmake
 include(CheckCXXSourceRuns)
 
-# Function to check Z3's version
-function(check_z3_version z3_include z3_lib)
-  # The program that will be executed to print Z3's version.
-  file(
-    WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testz3.cpp
-    "#include <assert.h>
-        #include <z3.h>
-        int main() {
-          unsigned int major, minor, build, rev;
-          Z3_get_version(&major, &minor, &build, &rev);
-          printf(\"%u.%u.%u\", major, minor, build);
-          return 0;
-       }")
-
-  # Get lib path
-  get_filename_component(z3_lib_path ${z3_lib} PATH)
-
-  try_run(
-    Z3_RETURNCODE Z3_COMPILED ${CMAKE_BINARY_DIR}
-    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testz3.cpp
-    COMPILE_DEFINITIONS -I"${z3_include}" LINK_LIBRARIES -L${z3_lib_path} -lz3
-    RUN_OUTPUT_VARIABLE SRC_OUTPUT)
-
-  if(Z3_COMPILED)
-    string(REGEX REPLACE "([0-9]*\\.[0-9]*\\.[0-9]*)" "\\1" z3_version "${SRC_OUTPUT}")
-    set(Z3_VERSION_STRING
-        ${z3_version}
-        PARENT_SCOPE)
-  endif()
-endfunction(check_z3_version)
-
 # if Z3_ROOT is provided, check there first
 set(Z3_ROOT
     ""
@@ -74,27 +43,19 @@ if(NOT Z3_FOUND)
 
   unset(Z3_VERSION_STRING)
 
-  # Try to check version by compiling a small program that prints Z3's version
-  if(Z3_CXX_INCLUDE_DIRS AND Z3_LIBRARIES)
-    check_z3_version(${Z3_CXX_INCLUDE_DIRS} ${Z3_LIBRARIES})
-  endif()
-
-  if(NOT Z3_VERSION_STRING
-     AND (CMAKE_CROSSCOMPILING
-          AND Z3_INCLUDE_DIRS
-          AND EXISTS "${Z3_INCLUDE_DIRS}/z3_version.h"))
-
-    file(STRINGS "${Z3_INCLUDE_DIRS}/z3_version.h" z3_version_str
+  # Try to check version of Z3
+  if(Z3_CXX_INCLUDE_DIRS AND (EXISTS "${Z3_CXX_INCLUDE_DIRS}/z3_version.h"))
+    file(STRINGS "${Z3_CXX_INCLUDE_DIRS}/z3_version.h" z3_version_str
          REGEX "^#define[\t ]+Z3_MAJOR_VERSION[\t ]+.*")
-    string(REGEX REPLACE "^.*Z3_MAJOR_VERSION[\t ]+([0-9]).*$" "\\1" Z3_MAJOR "${z3_version_str}")
+    string(REGEX REPLACE "^.*Z3_MAJOR_VERSION[\t ]+([0-9]*).*$" "\\1" Z3_MAJOR "${z3_version_str}")
 
-    file(STRINGS "${Z3_INCLUDE_DIRS}/z3_version.h" z3_version_str
+    file(STRINGS "${Z3_CXX_INCLUDE_DIRS}/z3_version.h" z3_version_str
          REGEX "^#define[\t ]+Z3_MINOR_VERSION[\t ]+.*")
-    string(REGEX REPLACE "^.*Z3_MINOR_VERSION[\t ]+([0-9]).*$" "\\1" Z3_MINOR "${z3_version_str}")
+    string(REGEX REPLACE "^.*Z3_MINOR_VERSION[\t ]+([0-9]*).*$" "\\1" Z3_MINOR "${z3_version_str}")
 
-    file(STRINGS "${Z3_INCLUDE_DIRS}/z3_version.h" z3_version_str
+    file(STRINGS "${Z3_CXX_INCLUDE_DIRS}/z3_version.h" z3_version_str
          REGEX "^#define[\t ]+Z3_BUILD_NUMBER[\t ]+.*")
-    string(REGEX REPLACE "^.*Z3_BUILD_VERSION[\t ]+([0-9]).*$" "\\1" Z3_BUILD "${z3_version_str}")
+    string(REGEX REPLACE "^.*Z3_BUILD_NUMBER[\t ]+([0-9]*).*$" "\\1" Z3_BUILD "${z3_version_str}")
 
     set(Z3_VERSION_STRING ${Z3_MAJOR}.${Z3_MINOR}.${Z3_BUILD})
     unset(z3_version_str)
