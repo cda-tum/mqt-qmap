253587aa5d883304ecb715da9325af2c84de50df
Sarah <mail@sarah-schneider.at>
2022-09-28 14:36:08 +0200

    Updated python tests

--- test/python/test_cliffordsynthesis.py
+++ test/python/test_cliffordsynthesis.py
@@ -4,22 +4,22 @@ from qiskit import QuantumCircuit
 
 
 def test_cliffordsynthesis_simple_binary_search() -> None:
-    """Verify that the exact mapper works on a simple circuit that requires no swaps on a trivial initial layout."""
+    """Verify that the clifford synthesis works on a simple circuit that requires no actual optimization"""
     qc = QuantumCircuit(2)
     qc.h(0)
     qc.cx(0, 1)
 
-    qc_mapped, results = qmap.synthesize_clifford(qc, use_binarysearch=True)
+    qc_mapped, results = qmap.optimize_clifford(qc, strategy="minmax")
 
     assert results.mapped_circuit != ""
 
 
 def test_cliffordsynthesis_simple_minimizer() -> None:
-    """Verify that the exact mapper works on a simple circuit that requires no swaps on a trivial initial layout."""
+    """Verify that the clifford synthesis works on a simple circuit that requires no actual optimization"""
     qc = QuantumCircuit(2)
     qc.h(0)
     qc.cx(0, 1)
 
-    qc_mapped, results = qmap.synthesize_clifford(qc, use_binarysearch=False)
+    qc_mapped, results = qmap.optimize_clifford(qc, strategy="use_minimizer")
 
     assert results.mapped_circuit != ""
