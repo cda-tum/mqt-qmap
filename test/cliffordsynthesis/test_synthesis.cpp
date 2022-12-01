0823fe186426428aa9a11edf62b02ea70960b747
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-12-01 18:08:14 +0100

    ♻️ restructure and unify tests
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

---
+++ test/cliffordsynthesis/test_synthesis.cpp
@@ -0,0 +1,238 @@
+//
+// This file is part of the MQT QMAP library released under the MIT license.
+// See README.md or go to https://github.com/cda-tum/qmap for more information.
+//
+
+#include "cliffordsynthesis/CliffordSynthesizer.hpp"
+
+#include "gtest/gtest.h"
+
+namespace cs {
+
+struct TestConfiguration {
+  // given input (either as tableau or as circuit)
+  std::string description;
+  std::string initialTableau;
+  std::string targetTableau;
+  std::string initialCircuit;
+
+  // expected output
+  std::size_t expectedMinimalGates{};
+  std::size_t expectedMinimalDepth{};
+  std::size_t expectedMinimalGatesAtMinimalDepth{};
+  std::size_t expectedMinimalTwoQubitGates{};
+  std::size_t expectedMinimalGatesAtMinimalTwoQubitGates{};
+};
+
+inline void from_json(const nlohmann::json& j, TestConfiguration& test) {
+  test.description = j.at("description").get<std::string>();
+  if (j.contains("initial_tableau")) {
+    test.initialTableau = j.at("initial_tableau").get<std::string>();
+  }
+  if (j.contains("target_tableau")) {
+    test.targetTableau = j.at("target_tableau").get<std::string>();
+  }
+  if (j.contains("initial_circuit")) {
+    test.initialCircuit = j.at("initial_circuit").get<std::string>();
+  }
+
+  test.expectedMinimalGates = j.at("expected_minimal_gates").get<std::size_t>();
+  test.expectedMinimalDepth = j.at("expected_minimal_depth").get<std::size_t>();
+  test.expectedMinimalGatesAtMinimalDepth =
+      j.at("expected_minimal_gates_at_minimal_depth").get<std::size_t>();
+  test.expectedMinimalTwoQubitGates =
+      j.at("expected_minimal_two_qubit_gates").get<std::size_t>();
+  test.expectedMinimalGatesAtMinimalTwoQubitGates =
+      j.at("expected_minimal_gates_at_minimal_two_qubit_gates")
+          .get<std::size_t>();
+}
+
+static std::vector<TestConfiguration> getTests(const std::string& path) {
+  std::ifstream  input(path);
+  nlohmann::json j;
+  input >> j;
+  return j;
+}
+
+class SynthesisTest : public ::testing::TestWithParam<TestConfiguration> {
+protected:
+  void SetUp() override {
+    if (plog::get() == nullptr) {
+      util::init("", plog::info);
+    }
+
+    test = GetParam();
+
+    if (!test.initialCircuit.empty()) {
+      std::stringstream      ss(test.initialCircuit);
+      qc::QuantumComputation qc{};
+      qc.import(ss, qc::OpenQASM);
+      std::cout << "Initial circuit:\n" << qc;
+      targetTableau = Tableau(qc);
+      if (test.initialTableau.empty()) {
+        initialTableau = Tableau(qc.getNqubits());
+        synthesizer    = CliffordSynthesizer(qc);
+      } else {
+        initialTableau = Tableau(test.initialTableau);
+        std::cout << "Initial tableau:\n" << initialTableau;
+        synthesizer = CliffordSynthesizer(initialTableau, qc);
+      }
+    } else {
+      targetTableau = Tableau(test.targetTableau);
+      if (test.initialTableau.empty()) {
+        initialTableau = Tableau(targetTableau.getQubitCount());
+        synthesizer    = CliffordSynthesizer(targetTableau);
+      } else {
+        initialTableau = Tableau(test.initialTableau);
+        std::cout << "Initial tableau:\n" << initialTableau;
+        synthesizer = CliffordSynthesizer(initialTableau, targetTableau);
+      }
+    }
+    std::cout << "Target tableau:\n" << targetTableau;
+
+    config = Configuration();
+  }
+
+  void TearDown() override {
+    std::cout << "Results:\n" << results << "\n";
+
+    resultTableau = synthesizer.getResultTableau();
+    std::cout << "Resulting tableau:\n" << resultTableau;
+    EXPECT_EQ(resultTableau, targetTableau);
+
+    const auto& resultCircuit = synthesizer.getResultCircuit();
+    std::cout << "Resulting Circuit:\n" << resultCircuit;
+    consistencyCheck(resultCircuit);
+  }
+
+  void consistencyCheck(const qc::QuantumComputation& qc) const {
+    auto circuitTableau = initialTableau;
+    for (const auto& gate : qc) {
+      circuitTableau.applyGate(gate.get());
+    }
+    EXPECT_EQ(resultTableau, circuitTableau);
+  }
+
+  Tableau             initialTableau;
+  Tableau             targetTableau;
+  Configuration       config;
+  CliffordSynthesizer synthesizer;
+  Results             results;
+  Tableau             resultTableau;
+  TestConfiguration   test;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    Tableaus, SynthesisTest,
+    testing::ValuesIn(getTests("cliffordsynthesis/tableaus.json")),
+    [](const testing::TestParamInfo<SynthesisTest::ParamType>& info) {
+      return info.param.description;
+    });
+
+INSTANTIATE_TEST_SUITE_P(
+    Circuits, SynthesisTest,
+    testing::ValuesIn(getTests("cliffordsynthesis/circuits.json")),
+    [](const testing::TestParamInfo<SynthesisTest::ParamType>& info) {
+      return info.param.description;
+    });
+
+TEST_P(SynthesisTest, Gates) {
+  config.target = TargetMetric::GATES;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getGates(), test.expectedMinimalGates);
+}
+
+TEST_P(SynthesisTest, GatesMaxSAT) {
+  config.target    = TargetMetric::GATES;
+  config.useMaxSAT = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getGates(), test.expectedMinimalGates);
+}
+
+TEST_P(SynthesisTest, Depth) {
+  config.target = TargetMetric::DEPTH;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getDepth(), test.expectedMinimalDepth);
+}
+
+TEST_P(SynthesisTest, DepthMaxSAT) {
+  config.target    = TargetMetric::DEPTH;
+  config.useMaxSAT = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getDepth(), test.expectedMinimalDepth);
+}
+
+TEST_P(SynthesisTest, DepthMinimalGates) {
+  config.target                              = TargetMetric::DEPTH;
+  config.minimizeGatesAfterDepthOptimization = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getDepth(), test.expectedMinimalDepth);
+  EXPECT_EQ(results.getGates(), test.expectedMinimalGatesAtMinimalDepth);
+}
+
+TEST_P(SynthesisTest, DepthMinimalGatesMaxSAT) {
+  config.target                              = TargetMetric::DEPTH;
+  config.useMaxSAT                           = true;
+  config.minimizeGatesAfterDepthOptimization = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getDepth(), test.expectedMinimalDepth);
+  EXPECT_EQ(results.getGates(), test.expectedMinimalGatesAtMinimalDepth);
+}
+
+TEST_P(SynthesisTest, TwoQubitGates) {
+  config.target = TargetMetric::TWO_QUBIT_GATES;
+  config.tryHigherGateLimitForTwoQubitGateOptimization = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getTwoQubitGates(), test.expectedMinimalTwoQubitGates);
+}
+
+TEST_P(SynthesisTest, TwoQubitGatesMaxSAT) {
+  config.target = TargetMetric::TWO_QUBIT_GATES;
+  config.tryHigherGateLimitForTwoQubitGateOptimization = true;
+  config.useMaxSAT                                     = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getTwoQubitGates(), test.expectedMinimalTwoQubitGates);
+}
+
+TEST_P(SynthesisTest, TwoQubitGatesMinimalGates) {
+  config.target = TargetMetric::TWO_QUBIT_GATES;
+  config.tryHigherGateLimitForTwoQubitGateOptimization = true;
+  config.minimizeGatesAfterTwoQubitGateOptimization    = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getTwoQubitGates(), test.expectedMinimalTwoQubitGates);
+  EXPECT_EQ(results.getGates(),
+            test.expectedMinimalGatesAtMinimalTwoQubitGates);
+}
+
+TEST_P(SynthesisTest, TwoQubitGatesMinimalGatesMaxSAT) {
+  config.target = TargetMetric::TWO_QUBIT_GATES;
+  config.tryHigherGateLimitForTwoQubitGateOptimization = true;
+  config.minimizeGatesAfterTwoQubitGateOptimization    = true;
+  config.useMaxSAT                                     = true;
+  synthesizer.synthesize(config);
+  results = synthesizer.getResults();
+
+  EXPECT_EQ(results.getTwoQubitGates(), test.expectedMinimalTwoQubitGates);
+  EXPECT_EQ(results.getGates(),
+            test.expectedMinimalGatesAtMinimalTwoQubitGates);
+}
+
+} // namespace cs
