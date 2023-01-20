9e32cf9ba4b41a7f6805ff4ae6d3ff071c9e01a6
Elias Foramitti <mitti.leon@gmail.com>
2023-01-20 03:29:34 +0100

    remove commented out code

--- include/heuristic/HeuristicMapper.hpp
+++ include/heuristic/HeuristicMapper.hpp
@@ -139,8 +139,6 @@ public:
      */
     void applyTeleportation(const Edge& swap, Architecture& arch,
                             bool considerFidelity) {
-      // const auto& singleQubitFidelities = arch.getSingleQubitFidelities();
-      // const auto& twoQubitFidelities = arch.getFidelityTable();
 
       const auto q1 = qubits.at(swap.first);
       const auto q2 = qubits.at(swap.second);
