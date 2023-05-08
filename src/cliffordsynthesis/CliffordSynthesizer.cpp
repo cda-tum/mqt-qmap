ee3ae77cfbc7c846cf714a0a3ea40aefddd775fe
Tom Peham <pehamtom@gmx.at>
2023-05-08 13:11:53 +0200

    Revert "Avoid copy in synthesizeSubcircuit."
    This reverts commit a0db789862a2e28ac5f246f6d52381c429af1c5d.

--- src/cliffordsynthesis/CliffordSynthesizer.cpp
+++ src/cliffordsynthesis/CliffordSynthesizer.cpp
@@ -492,6 +492,6 @@ CliffordSynthesizer::synthesizeSubcircuit(
   synth.synthesize(config);
 
   [[maybe_unused]] const auto& h = synth.getResultCircuit();
-  return synth.resultCircuit;
+  return std::make_shared<qc::QuantumComputation>(synth.resultCircuit->clone());
 }
 } // namespace cs
