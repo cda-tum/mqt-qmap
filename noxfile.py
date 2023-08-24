d51da9dabf49c3708bdfab2a79c68a80ca2f604b
burgholzer <burgholzer@me.com>
2023-08-24 14:18:32 +0200

    🔧 set CI settings for Python tests in noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import argparse
 import os
+import sys
 from typing import TYPE_CHECKING
 
 import nox
@@ -46,6 +47,12 @@ def _run_tests(
 ) -> None:
     posargs = list(session.posargs)
     env = {"PIP_DISABLE_PIP_VERSION_CHECK": "1"}
+
+    if os.environ.get("CI", None):
+        # disable `-march=native` for CI builds
+        env["SKBUILD_CMAKE_DEFINES"] = "DEPLOY=ON"
+        if sys.platform == "win32":
+            env["SKBUILD_CMAKE_ARGS"] = "-T ClangCL"
 
     _extras = ["test", *extras]
     if "--cov" in posargs:
