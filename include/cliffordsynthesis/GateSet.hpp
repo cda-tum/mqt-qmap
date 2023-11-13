eb359898f0f9f53509b0d7f8217a395a9112690e
Tom Peham <pehamtom@gmx.at>
2023-11-13 12:32:35 +0100

    Always add the none gate.

--- include/cliffordsynthesis/GateSet.hpp
+++ include/cliffordsynthesis/GateSet.hpp
@@ -7,21 +7,58 @@
 
 #include "operations/OpType.hpp"
 
+#include <initializer_list>
 #include <vector>
 
 namespace cs {
-class GateSet : public std::vector<qc::OpType> {
-  static constexpr std::array<qc::OpType, 9> SINGLE_QUBIT_CLIFFORDS = {
-      qc::OpType::I,   qc::OpType::H,  qc::OpType::X,
-      qc::OpType::Y,   qc::OpType::Z,  qc::OpType::S,
-      qc::OpType::Sdg, qc::OpType::SX, qc::OpType::SXdg};
+class GateSet {
+  static constexpr std::array<qc::OpType, 10> SINGLE_QUBIT_CLIFFORDS = {
+      qc::OpType::I,    qc::OpType::H,   qc::OpType::X,   qc::OpType::Y,
+      qc::OpType::Z,    qc::OpType::S,   qc::OpType::Sdg, qc::OpType::SX,
+      qc::OpType::SXdg, qc::OpType::None};
+
+  using iterator       = typename std::vector<qc::OpType>::iterator;
+  using const_iterator = typename std::vector<qc::OpType>::const_iterator;
+
+private:
+  // Check if None is already contained and if not, append it
+  void appendNone() {
+    if (!containsGate<qc::OpType::None>()) {
+      gates.push_back(qc::OpType::None);
+    }
+  }
 
 public:
-  using std::vector<qc::OpType>::vector;
+  std::vector<qc::OpType> gates;
+
+  GateSet() { appendNone(); };
+
+  explicit GateSet(std::vector<qc::OpType> gateSet)
+      : gates(std::move(gateSet)) {
+    appendNone();
+  };
+
+  GateSet(GateSet const& gateSet) : gates(gateSet.gates) { appendNone(); };
+
+  GateSet(std::initializer_list<qc::OpType> gateSet) : gates(gateSet) {
+    appendNone();
+  }
+
+  GateSet& operator=(const GateSet& other) {
+    gates = other.gates;
+    appendNone();
+    return *this;
+  }
+
+  GateSet& operator=(std::initializer_list<qc::OpType> other) {
+    gates = other;
+    appendNone();
+    return *this;
+  }
 
   template <qc::OpType Gate> [[nodiscard]] bool containsGate() const {
     for (const auto& g : // NOLINT(readability-use-anyofallof)
-         *this) {
+         gates) {
       if (g == Gate) {
         return true;
       }
@@ -43,8 +80,8 @@ public:
     return containsGate<qc::OpType::SXdg>();
   }
   [[nodiscard]] std::size_t gateToIndex(const qc::OpType type) const {
-    for (std::size_t i = 0; i < this->size(); ++i) {
-      if (this->at(i) == type) {
+    for (std::size_t i = 0; i < gates.size(); ++i) {
+      if (gates.at(i) == type) {
         return i;
       }
     }
@@ -52,7 +89,7 @@ public:
   }
 
   [[nodiscard]] bool isValidGateSet() const {
-    return std::all_of(this->begin(), this->end(), [](const auto& g) {
+    return std::all_of(gates.begin(), gates.end(), [](const auto& g) {
       return std::find(SINGLE_QUBIT_CLIFFORDS.begin(),
                        SINGLE_QUBIT_CLIFFORDS.end(),
                        g) != SINGLE_QUBIT_CLIFFORDS.end();
@@ -78,11 +115,84 @@ public:
 
   [[nodiscard]] std::string toString() const {
     std::string result = "{";
-    for (const auto& g : *this) {
+    for (const auto& g : gates) {
       result += qc::toString(g) + ", ";
     }
     result += "}";
     return result;
   }
+
+  /**
+   * Pass-Through
+   */
+
+  // Iterators (pass-through)
+  auto               begin() noexcept { return gates.begin(); }
+  [[nodiscard]] auto begin() const noexcept { return gates.begin(); }
+  [[nodiscard]] auto cbegin() const noexcept { return gates.cbegin(); }
+  auto               end() noexcept { return gates.end(); }
+  [[nodiscard]] auto end() const noexcept { return gates.end(); }
+  [[nodiscard]] auto cend() const noexcept { return gates.cend(); }
+  auto               rbegin() noexcept { return gates.rbegin(); }
+  [[nodiscard]] auto rbegin() const noexcept { return gates.rbegin(); }
+  [[nodiscard]] auto crbegin() const noexcept { return gates.crbegin(); }
+  auto               rend() noexcept { return gates.rend(); }
+  [[nodiscard]] auto rend() const noexcept { return gates.rend(); }
+  [[nodiscard]] auto crend() const noexcept { return gates.crend(); }
+
+  // Capacity (pass-through)
+  [[nodiscard]] bool        empty() const noexcept { return gates.empty(); }
+  [[nodiscard]] std::size_t size() const noexcept { return gates.size(); }
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  [[nodiscard]] std::size_t max_size() const noexcept {
+    return gates.max_size();
+  }
+  [[nodiscard]] std::size_t capacity() const noexcept {
+    return gates.capacity();
+  }
+
+  void reserve(const std::size_t newCap) { gates.reserve(newCap); }
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  void shrink_to_fit() { gates.shrink_to_fit(); }
+
+  // Modifiers (pass-through)
+  void clear() noexcept { gates.clear(); }
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  void     pop_back() { return gates.pop_back(); }
+  void     resize(std::size_t count) { gates.resize(count); }
+  iterator erase(const_iterator pos) { return gates.erase(pos); }
+  iterator erase(const_iterator first, const_iterator last) {
+    return gates.erase(first, last);
+  }
+
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  template <class T> void push_back(const T& gate) { gates.push_back(gate); }
+
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  template <class T, class... Args> void emplace_back(Args&&... args) {
+    gates.emplace_back(args...);
+  }
+
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  template <class T> void emplace_back(qc::OpType& gate) {
+    gates.emplace_back(gate);
+  }
+
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  template <class T> void emplace_back(qc::OpType&& gate) {
+    gates.emplace_back(gate);
+  }
+
+  template <class T> iterator insert(const_iterator pos, T&& gate) {
+    return gates.insert(pos, std::forward<T>(gate));
+  }
+
+  [[nodiscard]] const auto& at(const std::size_t i) const {
+    return gates.at(i);
+  }
+  [[nodiscard]] auto&       at(const std::size_t i) { return gates.at(i); }
+  [[nodiscard]] const auto& front() const { return gates.front(); }
+  [[nodiscard]] const auto& back() const { return gates.back(); }
 };
+
 } // namespace cs
