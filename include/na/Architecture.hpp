e5f132b7eab545f7414d74319db0e4285f575881
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-01-26 10:00:13 +0100

    :art: Tidy-up Architecture.hpp

--- include/na/Architecture.hpp
+++ include/na/Architecture.hpp
@@ -6,36 +6,82 @@
 #pragma once
 
 #include "Definitions.hpp"
+#include "OpType.hpp"
 
+#include <cstdint>
+#include <cstdlib>
 #include <map>
+#include <set>
 #include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 namespace na {
 
+/**
+ * @brief The scope of an operation (Global or Local)
+ */
 enum class Scope { Global, Local };
 
+/**
+ * @brief The type of a site (SLM or AOD)
+ * @details SLM comprises both AOD and SLM; AOD denotes AOD only.
+ */
 enum class Type { SLM, AOD };
-static std::map<std::string, Type> stringToType = { {"SLM", Type::SLM}, {"AOD", Type::AOD} };
-Type getTypeOfString(std::string& s) {
-  auto it = stringToType.find(s);
+static std::map<std::string, Type> const STRING_TO_TYPE = {{"SLM", Type::SLM},
+                                                           {"AOD", Type::AOD}};
+/**
+ * @brief Get the Type of a site (SLM or AOD) from a string
+ *
+ * @param s the name
+ * @return Type
+ */
+inline Type getTypeOfString(std::string& s) {
+  auto it = STRING_TO_TYPE.find(s);
   return it->second;
 }
 
+/**
+ * @brief Class to store two-dimensional coordinates
+ */
 class Point {
 public:
-  const std::uint16_t x;
-  const std::uint16_t y;
+  std::uint16_t x;
+  std::uint16_t y;
   Point(std::uint16_t x, std::uint16_t y) : x(x), y(y){};
-  inline Point operator-(Point p) { return Point(x - p.x, y - p.y); }
-  inline Point operator+(Point p) { return Point(x + p.x, y + p.y); }
-  [[nodiscard]] auto length() { return std::abs(x * x + y * y); }
+  inline Point operator-(Point&& p) {
+    x -= p.x;
+    y -= p.y;
+    return *this;
+  }
+  inline Point operator+(Point&& p) {
+    x += p.x;
+    y += p.y;
+    return *this;
+  }
+  [[nodiscard]] auto length() const { return std::abs(x * x + y * y); }
 };
 
-// template <typename Value> using Grid = std::vector<std::vector<Value>>;
-using Index  = size_t;
-using Zone   = Index;
-using Site   = std::tuple<Point, Zone, Type>;
-using Value  = qc::fp;
+/**
+ * @brief For Indices
+ */
+using Index = std::size_t;
+/**
+ * @brief The zones are just stored as int
+ */
+using Zone = Index;
+/**
+ * @brief A site is defined by a position, a zone, and a type
+ */
+using Site = std::tuple<Point, Zone, Type>;
+/**
+ * @brief Any double-valued property
+ */
+using Value = qc::fp;
+/**
+ * @brief Any information on numbers of somethin
+ */
 using Number = std::uint16_t;
 
 class Architecture {
@@ -51,26 +97,58 @@ public:
    */
   class DecoherenceTimes {
   public:
-    Value const t1;
-    Value const t2;
-    Value const tEff;
+    Value t1                              = 0;
+    Value t2                              = 0;
+    Value tEff                            = 0;
+    DecoherenceTimes()                    = default;
+    DecoherenceTimes(DecoherenceTimes& t) = default;
     inline DecoherenceTimes(Value t1, Value t2)
         : t1(t1), t2(t2), tEff(t1 * t2 / (t1 + t2)) {}
-    operator const double() const { return tEff; }
+    virtual ~DecoherenceTimes() = default;
+    explicit          operator double() const { return tEff; }
+    DecoherenceTimes& operator=(DecoherenceTimes&& t) noexcept {
+      if (this != &t) {
+        t1   = t.t1;
+        t2   = t.t2;
+        tEff = t.tEff;
+      }
+      return *this;
+    }
+  };
+  class Operation {
+  public:
+    qc::OpType type;  // the type of the gate, use also RY for global ones here
+    Scope      scope; // local or global
+    std::set<Zone> zones;    // the zones where the gate can be applied
+    Value          time;     // the time the gate takes to be applied
+    Value          fidelity; // the fidelity of the gate
+    Operation(qc::OpType type, Scope scope, std::set<Zone> zones, Value time,
+              Value fidelity)
+        : type(type), scope(scope), zones(std::move(zones)), time(time),
+          fidelity(fidelity) {}
   };
 
 protected:
-  std::string              name;
-  std::vector<std::string> zones;
-  std::vector<Site>        sites;
-  Number                   nSites;
-  DecoherenceTimes         decoherenceTimes;
-  Number                   nAods;
-  Value                    minAtomDistance;
-  Value                    interactionRadius;
+  std::string name; // the name of the architecure
+  std::vector<std::string>
+      zones; // a mapping from zones (int) to their name from the config
+  std::vector<Site>   sites;      // a vector of sites (Position, Zone, Type)
+  Number              nSites;     // number of sites
+  std::set<Operation> operations; // all possible operations, i.e. gate set
+  DecoherenceTimes    decoherenceTimes; // the decoherence characteristic
+  Number              nAods;            // number of AODs for atom movement
+  Number              nAodRows;         // possible rows per AOD
+  Number              nAodCols;         // possible columns per AOD
+  Value minAtomDistance;   // minimal distance that must be kept between atoms
+  Value interactionRadius; // the Rydberg radius
 
 public:
-  Architecture(std::string& filename);
+  /**
+   * @brief Import a new architecture from a file.
+   *
+   * @param filename The path to the JSON file
+   */
+  explicit Architecture(std::string& filename);
   virtual ~Architecture() = default;
   [[nodiscard]] inline auto getName() { return name; }
   [[nodiscard]] inline auto getNZones() { return zones.size(); }
@@ -87,10 +165,10 @@ public:
   }
   [[nodiscard]] inline auto getDecoherenceTimes() { return decoherenceTimes; }
   [[nodiscard]] inline auto getNAods() { return nAods; }
+  [[nodiscard]] inline auto getNAodRows() { return nAodRows; }
+  [[nodiscard]] inline auto getNAodCols() { return nAodCols; }
   [[nodiscard]] inline auto getMinAtomDistance() { return minAtomDistance; }
   [[nodiscard]] inline auto getInteractionRadius() { return interactionRadius; }
-  [[nodiscard]] static Architecture fromJSON(const std::string& filename);
-  [[nodiscard]] static Architecture fromJSON(std::ifstream& fs);
 
   [[nodiscard]] auto getDistance(Index i, Index j) {
     return (getPos(j) - getPos(i)).length();
