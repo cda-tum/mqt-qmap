423426f51a7db741bad73eeedcd5f225af46d672
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-01-26 11:31:23 +0100

    :adhesive_bandage: Fix all remaining bugs in Architecture class

--- src/na/Architecture.cpp
+++ src/na/Architecture.cpp
@@ -5,15 +5,24 @@
 
 #include "na/Architecture.hpp"
 
+#include "OpType.hpp"
 #include "nlohmann/json.hpp"
+#include "nlohmann/json_fwd.hpp"
 
+#include <exception>
+#include <filesystem>
 #include <fstream>
 #include <map>
+#include <set>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 
 namespace na {
+std::string getDirnameOf(std::string& filename) {
+  return std::filesystem::path(filename).parent_path().u8string();
+}
+
 Architecture::Architecture(std::string& filename) {
   std::ifstream  fs(filename);
   nlohmann::json data;
@@ -24,14 +33,20 @@ Architecture::Architecture(std::string& filename) {
     fs >> data;
     name = data["name"];
     // load CSV
-    std::string                 gridFname = data["grid"];
-    std::ifstream               gridFs(gridFname);
+    std::stringstream ss;
+    ss << getDirnameOf(filename) << data["grid"];
+    std::ifstream gridFs(ss.str());
+    // auxiliary variables
     std::string                 line;
-    std::map<std::string, Zone> nameToZone();
+    std::map<std::string, Zone> nameToZone;
 
     while (std::getline(gridFs, line)) {  // read one line
       std::stringstream lineStream(line); // make a stream of this line
-      std::string       sAddr, sX, sY, sZone, sType;
+      std::string       sAddr;
+      std::string       sX;
+      std::string       sY;
+      std::string       sZone;
+      std::string       sType;
 
       std::getline(lineStream, sAddr, ','); // read until the next separator
       std::getline(lineStream, sX, ',');
@@ -39,16 +54,33 @@ Architecture::Architecture(std::string& filename) {
       std::getline(lineStream, sZone, ',');
       std::getline(lineStream, sType, ',');
 
-      if( nameToZone.try_emplace(sZone, nameToZone.count())) { // FIXME
+      if (nameToZone.try_emplace(sZone, nameToZone.size()).second) {
         zones.emplace_back(sZone);
       }
-      sites.emplace_back(Point(stoi(sX), stoi(sY)), nameToZone.find(sZone), getTypeOfString(sType));
+      sites.emplace_back(
+          Point(stoi(sX), stoi(sY)), // make point from x and y (convert to int)
+          nameToZone.find(sZone)->second, // find the id of the zone
+          getTypeOfString(sType));        // convert to type
     }
     nSites = sites.size();
     // load rest of JSON
+    for (auto const& op : data["operations"]) {
+      qc::OpType const ty = qc::opTypeFromString(op["name"]);
+      Scope const      sc = getScopeOfString(op["type"]);
+      std::set<Zone>   zo = {};
+      for (auto const& zs : op["zones"]) {
+        zo.emplace(nameToZone.find(zs)->second);
+      }
+      Value const fi   = op["fidelity"];
+      Value const ti   = op["time"];
+      Operation const o = {ty, sc, zo, ti, fi};
+      operations.emplace(ty, o);
+    }
     decoherenceTimes = Architecture::DecoherenceTimes(
-        data["decoherence"]["t1"], data["decoherence"]["t2"]); // FIXME
+        data["decoherence"]["t1"], data["decoherence"]["t2"]);
     nAods             = data["AOD"]["number"];
+    nAodRows          = data["AOD"]["rows"];
+    nAodCols          = data["AOD"]["columns"];
     minAtomDistance   = data["AOD"]["minAtomDistance"];
     interactionRadius = data["interactionRadius"];
   } catch (std::exception& e) {
