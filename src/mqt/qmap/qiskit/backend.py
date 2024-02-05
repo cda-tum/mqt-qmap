752a66adec8cf4792c1b1f88faf39bdff72d3140
Lukas Burgholzer <burgholzer@me.com>
2024-02-05 23:41:17 +0100

    🚨 adapt for Qiskit 0.46.0 (#422)
    ## Description
    
    This PR updates the project to work with the latest Qiskit version. In
    turn, and in preparation for the 1.0 release of Qiskit, it adapts the
    project to the expected removals already. While this raises the minimum
    supported Qiskit version to 0.46.0, it should make switching to 1.0 as
    easy as possible.
    
    ## Checklist:
    
    <!---
    This checklist serves as a reminder of a couple of things that ensure
    your pull request will be merged swiftly.
    -->
    
    - [x] The pull request only contains commits that are related to it.
    - [x] I have added appropriate tests and documentation.
    - [x] I have made sure that all CI jobs on GitHub pass.
    - [x] The pull request introduces no new warnings and follows the
    project's style guidelines.
    
    ---------
    
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/qmap/qiskit/backend.py
+++ src/mqt/qmap/qiskit/backend.py
@@ -4,10 +4,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from qiskit.providers import Backend, BackendV1, BackendV2
+from qiskit.providers import Backend, BackendV1, BackendV2, BackendV2Converter
 
 if TYPE_CHECKING:
-    from qiskit.providers.models import BackendProperties
     from qiskit.transpiler import Target
 
 from mqt.qmap import Architecture
@@ -24,55 +23,11 @@ def import_backend(backend: Backend) -> Architecture:
 
     """
     if isinstance(backend, BackendV1):
-        return import_backend_v1(backend)
+        return import_backend_v2(BackendV2Converter(backend))
     if isinstance(backend, BackendV2):
         return import_backend_v2(backend)
     msg = f"Backend type {type(backend)} not supported."
     raise TypeError(msg)
-
-
-def import_backend_properties(backend_properties: BackendProperties) -> Architecture.Properties:
-    """Import backend properties from qiskit.providers.models.BackendProperties.
-
-    Args:
-        backend_properties: The backend properties to import.
-
-    Returns:
-        The imported backend properties as an Architecture.Properties object.
-    """
-    props = Architecture.Properties()
-    props.name = backend_properties.backend_name
-    props.num_qubits = len(backend_properties.qubits)
-    for qubit in range(props.num_qubits):
-        props.set_t1(qubit, backend_properties.t1(qubit))
-        props.set_t2(qubit, backend_properties.t2(qubit))
-        props.set_frequency(qubit, backend_properties.frequency(qubit))
-        props.set_readout_error(qubit, backend_properties.readout_error(qubit))
-
-    for gate in backend_properties.gates:
-        if gate.gate == "reset":
-            continue
-
-        if len(gate.qubits) == 1:
-            props.set_single_qubit_error(
-                gate.qubits[0], gate.gate, backend_properties.gate_error(gate.gate, gate.qubits)
-            )
-        elif len(gate.qubits) == 2:
-            props.set_two_qubit_error(
-                gate.qubits[0], gate.qubits[1], backend_properties.gate_error(gate.gate, gate.qubits), gate.gate
-            )
-    return props
-
-
-def import_backend_v1(backend: BackendV1) -> Architecture:
-    """Import a backend from qiskit.providers.BackendV1."""
-    arch = Architecture()
-    arch.name = backend.name()
-    arch.num_qubits = backend.configuration().n_qubits
-    arch.coupling_map = {(c[0], c[1]) for c in backend.configuration().coupling_map}
-    arch.properties = import_backend_properties(backend.properties())
-
-    return arch
 
 
 def import_target(target: Target) -> Architecture.Properties:
--- test/python/test_exact_mapper.py
+++ test/python/test_exact_mapper.py
@@ -2,14 +2,21 @@
 
 from __future__ import annotations
 
+import pytest
 from qiskit import QuantumCircuit
-from qiskit.providers.fake_provider import FakeLondon
+from qiskit.providers.fake_provider import GenericBackendV2
 
 from mqt import qmap
 from mqt.qcec import verify
 
 
-def test_exact_no_swaps_trivial_layout() -> None:
+@pytest.fixture()
+def backend() -> GenericBackendV2:
+    """Return a test backend."""
+    return GenericBackendV2(num_qubits=5, coupling_map=[[0, 1], [1, 0], [1, 2], [2, 1], [1, 3], [3, 1], [3, 4], [4, 3]])
+
+
+def test_exact_no_swaps_trivial_layout(backend: GenericBackendV2) -> None:
     """Verify that the exact mapper works on a simple circuit that requires no swaps on a trivial initial layout."""
     qc = QuantumCircuit(3)
     qc.h(0)
@@ -17,7 +24,7 @@ def test_exact_no_swaps_trivial_layout() -> None:
     qc.cx(1, 2)
     qc.measure_all()
 
-    qc_mapped, results = qmap.compile(qc, arch=FakeLondon(), method="exact")
+    qc_mapped, results = qmap.compile(qc, arch=backend, method="exact")
     assert results.timeout is False
     assert results.mapped_circuit
     assert results.output.swaps == 0
@@ -26,7 +33,7 @@ def test_exact_no_swaps_trivial_layout() -> None:
     assert result.considered_equivalent() is True
 
 
-def test_exact_no_swaps_non_trivial_layout() -> None:
+def test_exact_no_swaps_non_trivial_layout(backend: GenericBackendV2) -> None:
     """Verify that the exact mapper works on a simple circuit that requires a non-trivial layout to achieve no swaps."""
     qc = QuantumCircuit(4)
     qc.h(0)
@@ -35,7 +42,7 @@ def test_exact_no_swaps_non_trivial_layout() -> None:
     qc.cx(0, 3)
     qc.measure_all()
 
-    qc_mapped, results = qmap.compile(qc, arch=FakeLondon(), method="exact")
+    qc_mapped, results = qmap.compile(qc, arch=backend, method="exact")
 
     assert results.timeout is False
     assert results.mapped_circuit
@@ -45,7 +52,7 @@ def test_exact_no_swaps_non_trivial_layout() -> None:
     assert result.considered_equivalent() is True
 
 
-def test_exact_non_trivial_swaps() -> None:
+def test_exact_non_trivial_swaps(backend: GenericBackendV2) -> None:
     """Verify that the exact mapper works on a simple circuit that requires at least a single SWAP."""
     qc = QuantumCircuit(3)
     qc.h(0)
@@ -54,7 +61,7 @@ def test_exact_non_trivial_swaps() -> None:
     qc.cx(2, 0)
     qc.measure_all()
 
-    qc_mapped, results = qmap.compile(qc, arch=FakeLondon(), method="exact")
+    qc_mapped, results = qmap.compile(qc, arch=backend, method="exact")
 
     assert results.timeout is False
     assert results.mapped_circuit
