1c0194602ede6fc1d58b6da1d9360c1abaa94782
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-02-08 09:49:37 +0100

    🚸 define interface of mapper

--- include/na/NeutralAtomMapper.hpp
+++ include/na/NeutralAtomMapper.hpp
@@ -6,20 +6,31 @@
 #pragma once
 
 #include "QuantumComputation.hpp"
-#include "na/Architecture.hpp"
+#include "Architecture.hpp"
+#include "Configuration.hpp"
+#include <cstdint>
 
 namespace na {
 
 class NeutralAtomMapper {
+public:
+  struct Statistics {
+    std::uint32_t numInitialGates = 0;
+    std::uint32_t numMappedGates = 0;
+    qc::fp mappingTime = 0.0;
+  };
 protected:
   qc::QuantumComputation initialQc{};
   qc::QuantumComputation mappedQc{};
   na::Architecture arch;
+  na::Configuration config;
+  na::NeutralAtomMapper::Statistics stats {};
 public:
-  explicit NeutralAtomMapper(const Architecture& arch) : arch(arch) {}
+  explicit NeutralAtomMapper(const Architecture& arch, const Configuration& config) : arch(arch), config(config) {}
   virtual ~NeutralAtomMapper() = default;
 
   virtual void map(const qc::QuantumComputation& qc) = 0;
-  [[nodiscard]] const qc::QuantumComputation& getResult() const { return mappedQc; }
+  [[nodiscard]] auto getResult() const -> const qc::QuantumComputation& { return mappedQc; }
+  [[nodiscard]] auto getStats() const -> const na::NeutralAtomMapper::Statistics& { return stats; }
 };
 } // namespace na
