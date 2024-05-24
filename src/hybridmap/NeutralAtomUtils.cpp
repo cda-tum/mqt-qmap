6df309e7c4cb1f05a195e8ef312e02b8cb0b041e
Ludwig Schmid <ludwig.s.schmid@tum.de>
2024-05-24 18:40:22 +0200

    🎨 updated file headers

--- src/hybridmap/NeutralAtomUtils.cpp
+++ src/hybridmap/NeutralAtomUtils.cpp
@@ -1,5 +1,6 @@
 //
-// Created by Ludwig Schmid on 16.10.23.
+// This file is part of the MQT QMAP library released under the MIT license.
+// See README.md or go to https://github.com/cda-tum/qmap for more information.
 //
 
 #include "hybridmap/NeutralAtomUtils.hpp"
