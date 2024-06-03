c6fea2d7d8a4ede91b4d0bca1e69bc90ff3ae0bd
burgholzer <burgholzer@me.com>
2024-06-03 16:50:32 +0200

    ♻️ parametrize Python tests
    Signed-off-by: burgholzer <burgholzer@me.com>

--- test/python/test_hybridmap.py
+++ test/python/test_hybridmap.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import json
 from pathlib import Path
 
 import pytest
@@ -10,68 +9,36 @@ from qiskit import QuantumCircuit
 
 from mqt.qmap import HybridMapperParameters, HybridNAMapper, NeutralAtomHybridArchitecture
 
-json_data = {
-    "name": "rubidium",
-    "properties": {
-        "nRows": 5,
-        "nColumns": 6,
-        "nAods": 1,
-        "nAodCoordinates": 5,
-        "interQubitDistance": 3,
-        "minimalAodDistance": 0.1,
-        "interactionRadius": 3,
-        "blockingFactor": 1,
-    },
-    "parameters": {
-        "nQubits": 20,
-        "gateTimes": {
-            "none": 0.5,
-            "rx": 0.5,
-            "rz": 0.5,
-            "h": 0.5,
-            "x": 0.5,
-            "p": 0.001,
-            "cx": 0.7,
-            "cz": 0.2,
-            "ccz": 0.4,
-            "cccz": 0.6,
-            "ccccz": 0.8,
-            "mcx": 2.5,
-        },
-        "gateAverageFidelities": {
-            "none": 0.999,
-            "h": 0.999,
-            "x": 0.999,
-            "rx": 0.999,
-            "rz": 0.999,
-            "p": 0.999,
-            "cz": 0.995,
-            "ccz": 0.95,
-            "cccz": 0.95,
-            "ccccz": 0.95,
-            "cccccz": 0.95,
-            "mcx": 0.95,
-        },
-        "decoherenceTimes": {"t1": 100000000, "t2": 1500000},
-        "shuttlingTimes": {"move": 0.55, "aod_move": 0.55, "aod_activate": 20, "aod_deactivate": 20},
-        "shuttlingAverageFidelities": {"move": 1, "aod_move": 1, "aod_activate": 1, "aod_deactivate": 1},
-    },
-}
+arch_dir = Path(__file__).parent.parent / "hybridmap" / "architectures"
+circuit_dir = Path(__file__).parent.parent / "hybridmap" / "circuits"
 
 
 @pytest.mark.parametrize(
+    "circuit_filename",
+    [
+        "dj_nativegates_rigetti_qiskit_opt3_10.qasm",
+        "modulo_2.qasm",
+        "multiply_2.qasm",
+        "qft_nativegates_rigetti_qiskit_opt3_10.qasm",
+        "random_nativegates_rigetti_qiskit_opt3_10.qasm",
+    ],
+)
+@pytest.mark.parametrize(
+    "arch_filename",
+    [
+        "rubidium.json",
+        "rubidium_hybrid.json",
+        "rubidium_shuttling.json",
+    ],
+)
+@pytest.mark.parametrize(
     ("lookahead_weight", "decay", "gate_shuttling_weight"), [(0.0, 0.1, 0.1), (0.0, 0.0, 0.1), (0.0, 1.0, 10)]
 )
-def test_hybrid_na_mapper(lookahead_weight, decay, gate_shuttling_weight):
+def test_hybrid_na_mapper(
+    circuit_filename: str, arch_filename: str, lookahead_weight: float, decay: float, gate_shuttling_weight: float
+) -> None:
     """Test the hybrid Neutral Atom mapper."""
-    # Create a temporary file
-    temp_file = Path("arch.json")
-    temp_file.write_text(json.dumps(json_data))
-
-    arch = NeutralAtomHybridArchitecture(str(temp_file))
-
-    # Delete the temporary file
-    temp_file.unlink()
+    arch = NeutralAtomHybridArchitecture(str(arch_dir / arch_filename))
 
     params = HybridMapperParameters(
         lookahead_weight_moves=lookahead_weight,
@@ -82,13 +49,7 @@ def test_hybrid_na_mapper(lookahead_weight, decay, gate_shuttling_weight):
     mapper = HybridNAMapper(arch, params=params)
 
     # Create a simple circuit
-    qc = QuantumCircuit(6)
-    qc.h(0)
-    qc.cx(0, 1)
-    qc.cx(0, 2)
-    qc.cx(0, 3)
-    qc.cx(0, 4)
-    qc.cx(0, 5)
+    qc = QuantumCircuit.from_qasm_file(str(circuit_dir / circuit_filename))
 
     mapper.map(qc)
     results = mapper.schedule(create_animation_csv=False)
