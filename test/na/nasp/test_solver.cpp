fb85907c0f982239a80bea04fb562b09f40d5a95
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-08-06 12:44:42 +0200

    ✅ Add another test for solver

--- test/na/nasp/test_solver.cpp
+++ test/na/nasp/test_solver.cpp
@@ -7,6 +7,44 @@
 #include <gtest/gtest.h>
 #include <string>
 
+TEST(Optimizer, SteaneDoubleSidedStorage) {
+  const std::string qasm = R"(OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[7];
+h q[0];
+h q[1];
+h q[2];
+h q[3];
+h q[4];
+h q[5];
+h q[6];
+cz q[0],q[6];
+cz q[1],q[3];
+cz q[4],q[5];
+cz q[0],q[4];
+cz q[5],q[6];
+cz q[1],q[2];
+cz q[0],q[2];
+cz q[3],q[5];
+cz q[1],q[4];
+h q[2];
+h q[3];
+h q[4];
+h q[6];
+)";
+  const auto&       circ = qc::QuantumComputation::fromQASM(qasm);
+  // create solver
+  na::NASolver solver;
+  solver.init(3, 7, 2, 3, 2, 2, 2, 2, 2, 4);
+  // get operations for solver
+  const auto& pairs =
+      na::SolverFactory::getOpsForSolver(circ, {qc::Z, 1}, true);
+  // solve
+  const auto result = solver.solve(pairs, circ.getNqubits(), 4, false, true);
+  EXPECT_TRUE(result.isSat());
+  EXPECT_EQ(result.numStages(), 4);
+}
+
 TEST(Solver, SteaneBottomStorage) {
   const std::string qasm = R"(OPENQASM 2.0;
 include "qelib1.inc";
