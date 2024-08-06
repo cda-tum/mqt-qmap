0cb6d2ad0ea179a2beb9544df4c1539cbb4c1f09
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-08-06 15:38:59 +0200

    ✅ Add test for YAML in- and output

--- test/na/nasp/test_solver.cpp
+++ test/na/nasp/test_solver.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <gtest/gtest.h>
 #include <string>
+#include <yaml-cpp/node/parse.h>
 
 TEST(Solver, SteaneDoubleSidedStorage) {
   const std::string qasm = R"(OPENQASM 2.0;
@@ -108,7 +109,7 @@ h q[6];
   }
 }
 
-TEST(Solver, NoShieldingFixedTransfersFixedOrder) {
+TEST(Solver, NoShieldingFixedOrder) {
   const std::string qasm = R"(OPENQASM 2.0;
 include "qelib1.inc";
 qreg q[7];
@@ -142,6 +143,129 @@ h q[6];
       na::SolverFactory::getOpsForSolver(circ, {qc::Z, 1}, true);
   // solve
   const auto result = solver.solve(
-      pairs, static_cast<std::uint16_t>(circ.getNqubits()), 3, 0, true, false);
+      pairs, static_cast<std::uint16_t>(circ.getNqubits()), 3, true, false);
   EXPECT_TRUE(result.isSat());
+}
+
+TEST(Solver, FixedTransfer) {
+  const std::string qasm = R"(OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[7];
+h q[0];
+h q[1];
+h q[2];
+h q[3];
+h q[4];
+h q[5];
+h q[6];
+cz q[0],q[6];
+cz q[1],q[3];
+cz q[4],q[5];
+cz q[0],q[4];
+cz q[5],q[6];
+cz q[1],q[2];
+cz q[0],q[2];
+cz q[3],q[5];
+cz q[1],q[4];
+h q[2];
+h q[3];
+h q[4];
+h q[6];
+)";
+  const auto&       circ = qc::QuantumComputation::fromQASM(qasm);
+  // create solver
+  na::NASolver solver;
+  solver.init(3, 7, 2, 3, 2, 2, 2, 2, 2, 4);
+  // get operations for solver
+  const auto& pairs =
+      na::SolverFactory::getOpsForSolver(circ, {qc::Z, 1}, true);
+  // solve
+  const auto result = solver.solve(
+      pairs, static_cast<std::uint16_t>(circ.getNqubits()), 5, 2, false, true);
+  EXPECT_TRUE(result.isSat());
+}
+
+TEST(Solver, Unsat) {
+  const std::string qasm = R"(OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[7];
+h q[0];
+h q[1];
+h q[2];
+h q[3];
+h q[4];
+h q[5];
+h q[6];
+cz q[0],q[6];
+cz q[1],q[3];
+cz q[4],q[5];
+cz q[0],q[4];
+cz q[5],q[6];
+cz q[1],q[2];
+cz q[0],q[2];
+cz q[3],q[5];
+cz q[1],q[4];
+h q[2];
+h q[3];
+h q[4];
+h q[6];
+)";
+  const auto&       circ = qc::QuantumComputation::fromQASM(qasm);
+  // create solver
+  na::NASolver solver;
+  solver.init(3, 7, 2, 3, 2, 2, 2, 2, 2, 4);
+  // get operations for solver
+  const auto& pairs =
+      na::SolverFactory::getOpsForSolver(circ, {qc::Z, 1}, true);
+  // solve
+  const auto result = solver.solve(
+      pairs, static_cast<std::uint16_t>(circ.getNqubits()), 3, false, true);
+  EXPECT_FALSE(result.isSat());
+}
+
+TEST(Solver, Exceptions) {
+  na::NASolver solver;
+  EXPECT_THROW(solver.init(3, 7, 2, 3, 2, 2, 2, 2, 2, 7),
+               std::invalid_argument);
+  solver.init(3, 7, 2, 3, 2, 2, 2, 2, 0, 7);
+  EXPECT_THROW(std::ignore = solver.solve({{0, 1}}, 3, 1, false, true), std::invalid_argument);
+}
+
+TEST(Solver, YAMLRoundTrip) {
+  const std::string qasm = R"(OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[7];
+h q[0];
+h q[1];
+h q[2];
+h q[3];
+h q[4];
+h q[5];
+h q[6];
+cz q[0],q[6];
+cz q[1],q[3];
+cz q[4],q[5];
+cz q[0],q[4];
+cz q[5],q[6];
+cz q[1],q[2];
+cz q[0],q[2];
+cz q[3],q[5];
+cz q[1],q[4];
+h q[2];
+h q[3];
+h q[4];
+h q[6];
+)";
+  const auto&       circ = qc::QuantumComputation::fromQASM(qasm);
+  // create solver
+  na::NASolver solver;
+  solver.init(3, 7, 2, 3, 2, 2, 2, 2, 2, 4);
+  // get operations for solver
+  const auto& pairs =
+      na::SolverFactory::getOpsForSolver(circ, {qc::Z, 1}, true);
+  // solve
+  const auto result = solver.solve(
+      pairs, static_cast<std::uint16_t>(circ.getNqubits()), 4, false, true);
+  const auto resultRT = na::NASolver::Result::fromYAML(YAML::Load(result.yaml())); // Round-Tripped result
+  EXPECT_EQ(resultRT, result);
 }
