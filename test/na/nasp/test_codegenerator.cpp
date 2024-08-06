c78843e0a87595d23c95429b0b9c8de640b5a461
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-08-06 13:39:48 +0200

    ✅ Add test for CodeGenerator

---
+++ test/na/nasp/test_codegenerator.cpp
@@ -0,0 +1,43 @@
+#include "CodeGenerator.hpp"
+#include "SolverFactory.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(CodeGenerator, Generate) {
+  const std::string qasm = R"(OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[7];
+h q[0];
+h q[1];
+h q[2];
+h q[3];
+h q[4];
+h q[5];
+h q[6];
+cz q[0],q[6];
+cz q[1],q[3];
+cz q[4],q[5];
+cz q[0],q[4];
+cz q[5],q[6];
+cz q[1],q[2];
+cz q[0],q[2];
+cz q[3],q[5];
+cz q[1],q[4];
+h q[2];
+h q[3];
+h q[4];
+h q[6];
+)";
+  const auto&       circ = qc::QuantumComputation::fromQASM(qasm);
+  // create solver
+  na::NASolver solver;
+  solver.init(3, 7, 2, 3, 2, 2, 2, 2, 2, 4);
+  // get operations for solver
+  const auto& pairs =
+      na::SolverFactory::getOpsForSolver(circ, {qc::Z, 1}, true);
+  // solve
+  const auto result = solver.solve(
+      pairs, static_cast<std::uint16_t>(circ.getNqubits()), 4, false, true);
+  const auto & comp = na::CodeGenerator::generate(circ, result, 2, 2, 2, 4);
+  EXPECT_TRUE(comp.validateAODConstraints());
+}
