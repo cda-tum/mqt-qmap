e2b8cb13c3ec24d305de71b753eeef8321e3cb00
lsschmid <117631861+lsschmid@users.noreply.github.com>
2024-08-08 15:18:04 +0200

    👷  changed mqt-core branch

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -29,7 +29,7 @@ endif()
 # cmake-format: off
 set(MQT_CORE_VERSION 2.5.2
     CACHE STRING "MQT Core version")
-set(MQT_CORE_REV "7a9a20269462360d2c2a951647eb6fbf4e3094e2"
+set(MQT_CORE_REV "na-hybrid-extension"
     CACHE STRING "MQT Core identifier (tag, branch or commit hash)")
 set(MQT_CORE_REPO_OWNER "cda-tum"
     CACHE STRING "MQT Core repository owner (change when using a fork)")
