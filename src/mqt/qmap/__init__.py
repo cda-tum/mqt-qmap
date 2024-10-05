26e444bbd58e73697c5467f69a047e962bde48a9
burgholzer <burgholzer@me.com>
2024-10-05 10:50:54 +0200

    🔥 remove obsolete Python version check
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/qmap/__init__.py
+++ src/mqt/qmap/__init__.py
@@ -10,7 +10,7 @@ import os
 import sys
 from pathlib import Path
 
-if sys.platform == "win32" and sys.version_info > (3, 8, 0) and "Z3_ROOT" in os.environ:
+if sys.platform == "win32" and "Z3_ROOT" in os.environ:
     lib_path = Path(os.environ["Z3_ROOT"]) / "lib"
     if lib_path.exists():
         os.add_dll_directory(str(lib_path))
