a2f254527192d3a9405060b2c7bf1db708f68884
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-02-27 10:59:06 +0100

    Add docu

--- include/na/zonar/AStarSearch.hpp
+++ include/na/zonar/AStarSearch.hpp
@@ -12,24 +12,58 @@ namespace na {
 template <class Node, class ForwardIt>
 
 /**
- * @brief A* search algorithm
+ * @brief A* search algorithm for trees where neighbors are sorted by cost
  * @details A* is a graph traversal and path search algorithm that finds the
  * shortest path between a start node and a goal node. It evaluates nodes by
  * combining the cost to reach the node and the cost to get from the node to the
  * goal estimated by a heuristic function.
- * @param start the start node
- * @param getNeighbors a function that returns the neighbors of a node
- * @param isGoal a function that returns true if a node is the goal
+ * @par
+ * This implementation of the A* search algorithm has some particularities:
+ * - To increase performance for the special case of a tree, where there cannot
+ * be any cycles and a node can only be reached by one path, it does not keep
+ * visited nodes. This would require a hash set or similar data structure to
+ * store visited nodes and check if a node has already been visited. This
+ * check would take at least O(log(n)) time for a hash set and is superfluous
+ * for trees.
+ * - As a consequence of the first point, this implementation also does not
+ * check whether a node is already in the open set. This would also require an
+ * O(log(n)) check operation which is not necessary for trees as one path can
+ * only reach a node.
+ * - To keep the maintenance of the open set as simple as possible, the open set
+ * only stores one neighbor of its parent node at a time. To achieve this, the
+ * @p getNeighbors function must return the neighbors in increasing order of
+ * cost. The first neighbor is then the one with the lowest cost and is
+ * placed in the priority queue. If this neighbor is not the only one, it is
+ * placed in the queue as a special item meaning that it represents also all
+ * other neighbors with higher cost. When this item is popped from the queue,
+ * it is replaced by the next neighbor with higher cost.
+ * @note This implementation of A* search can only handle trees and not general
+ * graphs. This is because it does not keep track of visited nodes and therefore
+ * cannot detect cycles. Also for DAGs it may expand nodes multiple times when
+ * they can be reached by different paths from the start node.
+ * @note The function @p getNeighbors must return the neighbors of a node in a
+ * sorted order by cost. The neighbor with the lowest cost must be the first
+ * element in the range of neighbors.
+ * @note @p getHeuristic must be admissible, meaning that it never
+ * overestimates the cost to reach the goal from the current node calculated by
+ * @p getCost for every edge on the path.
+ * @note The calling program has to make sure that the pointers and iterators
+ * passed to this function are valid and that the iterators are not invalidated
+ * during the search, e.g., by calling one of the passed functions like @p
+ * getNeighbors.
+ * @param start a pointer to the start node
+ * @param getNeighbors a function that returns the neighbors of a node as an
+ * iterator pair, the first iterator must be the 'begin' iterator and the second
+ * iterator must be the 'end' iterator. The neighbors must be sorted by cost,
+ * see the note above.
+ * @param isGoal a function that returns true if a node is one of potentially
+ * multiple goals
  * @param getCost a function that returns the cost of moving from one node to
  * another
  * @param getHeuristic a function that returns the heuristic cost from the node
  * to any goal.
- * @return a vector of nodes representing the path from the start to a goal
- * @note @p getHeuristic must be admissible, meaning that it never
- * overestimates the cost to reach the goal from the current node calculated by
- * @p getCost for every edge on the path.
- * @note Note that there must be a hash function for the Node class defined.
- * It is also useful to define the == operator for the Node class.
+ * @return a vector of node pointers representing the path from the start to a
+ * goal
  */
 auto aStarTreeSearch(
     const Node* start,
@@ -43,10 +77,16 @@ auto aStarTreeSearch(
   //===--------------------------------------------------------------------===//
   // struct for items in the open set
   struct Item {
-    double priority; //< sum of cost and heuristic
-    double cost;
-    const Node* node;
+    double priority;  //< sum of cost and heuristic
+    double cost;      //< actual cost to reach the node
+    const Node* node; //< pointer to the node
+    // iterator pair to the siblings of a node with a 'begin' and 'end' iterator
+    // if the iterator is 'empty' the optional is set to 'nullopt' and then
+    // this item represents a regular item. If it is not empty, the item
+    // is a special item and represents the contained node together with all
+    // its more costly siblings.
     std::optional<std::pair<ForwardIt, ForwardIt>> siblings;
+    // pointer to the parent item to reconstruct the path in the end
     Item* parent;
     Item(const double priority, const double cost, const Node* node,
          ForwardIt siblings, ForwardIt end, Item* parent)
@@ -60,15 +100,22 @@ auto aStarTreeSearch(
   // compare function for the open set
   struct ItemCompare {
     bool operator()(const Item* a, const Item* b) const {
+      // this way, the item with the lowest priority is on top of the heap
       return a->priority > b->priority;
     }
   };
+  // vector of items to store all items and keep them alive also after they are
+  // popped from the open set.
+  // they are required alive to reconstruct the path in the end.
   std::vector<std::unique_ptr<Item>> items;
-  // open list of nodes to be evaluated as a minimum heap based on the priority
+  // open list of nodes to be evaluated as a minimum heap based on the priority.
+  // whenever an item is placed in the queue it is created in the vector `items`
+  // before and only a reference is placed in the queue
   std::priority_queue<Item*, std::vector<Item*>, ItemCompare> openSet;
-  openSet.emplace(
-      items.emplace_back(std::make_unique<Item>(getHeuristic(start), 0.0, start, nullptr))
-          .get());
+  openSet.emplace(items
+                      .emplace_back(std::make_unique<Item>(getHeuristic(start),
+                                                           0.0, start, nullptr))
+                      .get());
   //===--------------------------------------------------------------------===//
   // Perform A* search
   //===--------------------------------------------------------------------===//
@@ -92,6 +139,8 @@ auto aStarTreeSearch(
       const Node* nextSibling = *itm->siblings->first++;
       const auto cost = itm->parent->cost + getCost(itm->node, nextSibling);
       const auto heuristic = getHeuristic(nextSibling);
+      // if the sibling is the last one, create a regular item otherwise create
+      // a special item together with the advanced iterator
       if (itm->siblings->first == itm->siblings->second) {
         openSet.emplace(
             items
@@ -113,20 +162,25 @@ auto aStarTreeSearch(
     auto [it, end] = getNeighbors(itm->node);
     if (it != end) {
       const Node* firstNeighbor = *it++;
+      // getCost returns the cost for the edge from the current node (itm->node)
+      // to the neighbor (firstNeighbor).
+      // hence, the total cost is the cost to reach the current node (itm->cost)
+      // plus the cost to reach the neighbor (getCost(itm->node, firstNeighbor))
       const auto cost = itm->cost + getCost(itm->node, firstNeighbor);
       const auto heuristic = getHeuristic(firstNeighbor);
+      // if the neighbor is the last one, create a regular item otherwise create
+      // a special item together with the advanced iterator
       if (it == end) {
-      openSet.emplace(
-          items
-              .emplace_back(std::make_unique<Item>(cost + heuristic, cost,
-                                                   firstNeighbor, itm))
-              .get());
+        openSet.emplace(items
+                            .emplace_back(std::make_unique<Item>(
+                                cost + heuristic, cost, firstNeighbor, itm))
+                            .get());
       } else {
-      openSet.emplace(
-          items
-              .emplace_back(std::make_unique<Item>(cost + heuristic, cost,
-                                                   firstNeighbor, it, end, itm))
-              .get());
+        openSet.emplace(
+            items
+                .emplace_back(std::make_unique<Item>(
+                    cost + heuristic, cost, firstNeighbor, it, end, itm))
+                .get());
       }
     }
   }
