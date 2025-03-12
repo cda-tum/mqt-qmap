7e8a50dffd736d3747563a7f0bbad2781a45b96c
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-03-12 18:06:58 +0100

    🐛 Fix bug in reuse tests

--- test/na/azac/test_vm_reuse_analyzer.cpp
+++ test/na/azac/test_vm_reuse_analyzer.cpp
@@ -10,6 +10,73 @@
 #include <vector>
 
 namespace na {
+constexpr std::string_view architectureJson = R"({
+  "name": "asap_scheduler_architecture",
+  "storage_zones": [{
+    "zone_id": 0,
+    "slms": [{"id": 0, "site_separation": [3, 3], "r": 20, "c": 20, "location": [0, 0]}],
+    "offset": [0, 0],
+    "dimension": [60, 60]
+  }],
+  "entanglement_zones": [{
+    "zone_id": 0,
+    "slms": [
+      {"id": 1, "site_separation": [12, 10], "r": 4, "c": 4, "location": [5, 70]},
+      {"id": 2, "site_separation": [12, 10], "r": 4, "c": 4, "location": [7, 70]}
+    ],
+    "offset": [5, 70],
+    "dimension": [50, 40]
+  }],
+  "aods":[{"id": 0, "site_separation": 2, "r": 20, "c": 20}],
+  "arch_range": [[0, 0], [60, 110]],
+  "rydberg_range": [[[5, 70], [55, 110]]]
+})";
+class VMReuseAnalyzerAnalyzeTest : public ::testing::Test {
+protected:
+  Architecture architecture;
+  nlohmann::json config;
+  VMReuseAnalyzer analyzer;
+  VMReuseAnalyzerAnalyzeTest()
+      : architecture(nlohmann::json::parse(architectureJson)),
+        analyzer{architecture, config} {}
+};
+TEST_F(VMReuseAnalyzerAnalyzeTest, NoGates) {
+  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers;
+  EXPECT_THAT(analyzer.analyzeReuse(twoQubitGateLayers), ::testing::IsEmpty());
+}
+TEST_F(VMReuseAnalyzerAnalyzeTest, OneLayer) {
+  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers{
+      {{0, 1}}};
+  EXPECT_THAT(analyzer.analyzeReuse(twoQubitGateLayers), ::testing::IsEmpty());
+}
+TEST_F(VMReuseAnalyzerAnalyzeTest, NoChoice) {
+  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers{
+      {{0, 1}}, {{1, 2}}};
+  EXPECT_THAT(analyzer.analyzeReuse(twoQubitGateLayers),
+              ::testing::ElementsAre(::testing::UnorderedElementsAre(1U)));
+}
+TEST_F(VMReuseAnalyzerAnalyzeTest, Unique) {
+  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers{
+      {{0, 1}, {2, 3}, {4, 5}}, {{1, 2}, {3, 4}, {5, 7}}};
+  EXPECT_THAT(
+      analyzer.analyzeReuse(twoQubitGateLayers),
+      ::testing::ElementsAre(::testing::UnorderedElementsAre(1U, 3U, 5U)));
+}
+TEST(VMReuseAnalyzerTest, Config) {
+  Architecture architecture(nlohmann::json::parse(architectureJson));
+  nlohmann::json config = R"({
+  "vm_reuse_analyzer": {
+    "unknown_key": 42
+  }
+})"_json;
+  std::stringstream buffer;
+  std::streambuf* oldCout = std::cout.rdbuf(buffer.rdbuf());
+  std::ignore = VMReuseAnalyzer(architecture, config);
+  std::cout.rdbuf(oldCout);
+  EXPECT_EQ(buffer.str(),
+            "[WARN] Configuration for VMReuseAnalyzer contains an "
+            "unknown key: unknown_key. Ignoring.\n");
+}
 class VMReuseAnalyzerMaximumBipartiteMatchingTest : public ::testing::Test {
 protected:
   // We consider the following bipartite graph, where the nodes in the upper row
@@ -56,49 +123,5 @@ TEST(VMReuseAnalyzerMaximumBipartiteMatchingInvertedTest, Direct) {
       /* 3 -> */ {2, 3}};
   EXPECT_THAT(VMReuseAnalyzer::maximumBipartiteMatching(inverseSparseMatrix),
               ::testing::ElementsAre(0, 2, 1, 3));
-}
-class VMReuseAnalyzerAnalyzeTest : public ::testing::Test {
-protected:
-  Architecture architecture;
-  nlohmann::json config;
-  VMReuseAnalyzer analyzer;
-  VMReuseAnalyzerAnalyzeTest() : analyzer{architecture, config} {}
-};
-TEST_F(VMReuseAnalyzerAnalyzeTest, NoGates) {
-  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers;
-  EXPECT_THAT(analyzer.analyzeReuse(twoQubitGateLayers), ::testing::IsEmpty());
-}
-TEST_F(VMReuseAnalyzerAnalyzeTest, OneLayer) {
-  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers{
-      {{0, 1}}};
-  EXPECT_THAT(analyzer.analyzeReuse(twoQubitGateLayers), ::testing::IsEmpty());
-}
-TEST_F(VMReuseAnalyzerAnalyzeTest, NoChoice) {
-  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers{
-      {{0, 1}}, {{1, 2}}};
-  EXPECT_THAT(analyzer.analyzeReuse(twoQubitGateLayers),
-              ::testing::ElementsAre(::testing::UnorderedElementsAre(1U)));
-}
-TEST_F(VMReuseAnalyzerAnalyzeTest, Unique) {
-  std::vector<std::vector<std::pair<qc::Qubit, qc::Qubit>>> twoQubitGateLayers{
-      {{0, 1}, {2, 3}, {4, 5}}, {{1, 2}, {3, 4}, {5, 7}}};
-  EXPECT_THAT(
-      analyzer.analyzeReuse(twoQubitGateLayers),
-      ::testing::ElementsAre(::testing::UnorderedElementsAre(1U, 3U, 5U)));
-}
-TEST(VMReuseAnalyzerTest, Config) {
-  Architecture architecture;
-  nlohmann::json config = R"({
-  "vm_reuse_analyzer": {
-    "unknown_key": 42
-  }
-})"_json;
-  std::stringstream buffer;
-  std::streambuf* oldCout = std::cout.rdbuf(buffer.rdbuf());
-  std::ignore = VMReuseAnalyzer(architecture, config);
-  std::cout.rdbuf(oldCout);
-  EXPECT_EQ(buffer.str(),
-            "[WARN] Configuration for VMReuseAnalyzer contains an "
-            "unknown key: unknown_key. Ignoring.\n");
 }
 } // namespace na
