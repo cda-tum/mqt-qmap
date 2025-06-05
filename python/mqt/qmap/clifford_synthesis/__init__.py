eb6148370ee480df5df1197fbd7215cfb77d503c
Daniel Haag <denialhaag@gmail.com>
2025-06-05 14:09:10 +0200

    Introduce empty Clifford module

---
+++ python/mqt/qmap/clifford_synthesis/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""Clifford synthesis module."""
+
+from __future__ import annotations
+
+from ..pyqmap import (
+    CliffordSynthesizer,
+    SynthesisConfiguration,
+    SynthesisResults,
+    Tableau,
+)
+from .util import optimize_clifford, synthesize_clifford
+
+__all__ = [
+    "CliffordSynthesizer",
+    "SynthesisConfiguration",
+    "SynthesisResults",
+    "Tableau",
+    "optimize_clifford",
+    "synthesize_clifford",
+]
+
+
+def __dir__() -> list[str]:
+    return __all__
