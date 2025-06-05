5e4d249e98435bfe41cbc7a849f40b17927c28eb
Daniel Haag <denialhaag@gmail.com>
2025-06-05 17:08:45 +0200

    Move hybrid mapper into its own module

--- test/python/test_hybridmap.py
+++ test/python/test_hybridmap.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import pytest
 from mqt.core import load
 
-from mqt.qmap.pyqmap import HybridMapperParameters, HybridNAMapper, NeutralAtomHybridArchitecture
+from mqt.qmap.hybrid_mapper import HybridMapperParameters, HybridNAMapper, NeutralAtomHybridArchitecture
 
 arch_dir = Path(__file__).parent.parent / "hybridmap" / "architectures"
 circuit_dir = Path(__file__).parent.parent / "hybridmap" / "circuits"
