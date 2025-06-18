f3214f2ff3ced614afb4e1f0e31bd15d686a6a12
Daniel Haag <denialhaag@gmail.com>
2025-06-18 15:51:40 +0200

    Also increase minimum version

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -48,7 +48,7 @@ if(BUILD_MQT_QMAP_BINDINGS)
 endif()
 
 # cmake-format: off
-set(MQT_CORE_MINIMUM_VERSION 3.0.0
+set(MQT_CORE_MINIMUM_VERSION 3.0.3
     CACHE STRING "MQT Core minimum version")
 set(MQT_CORE_VERSION 3.0.3
     CACHE STRING "MQT Core version")
